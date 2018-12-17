e47dbc3d3b7536bfce083e59d23660d5e4dc63aa
Tom Klinovsky <tom.klino@gmail.com>
2018-12-17 22:47:01 +0200

    added waitForReady flag

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const Redis = require('ioredis')
 
 module.exports = function redisClientFactoryInit(conf) {
-  async function createClient() {
+  async function createClient(opts) {
+    let { waitForReady = false } = opts || {}
     let redis = new Redis(conf)
     return redis.cluster('info')
       .then(() => {
         console.log("cluster mode detected")
-        return new Redis.Cluster([conf])
+        return waitForReady ?
+          waitForReadyPromise(new Redis.Cluster([conf])) :
+          new Redis.Cluster([conf])
       })
       .catch((e) => {
+        //single instance mode
+        //NOTE: readiness flag skipped, as client is after 'info' command
+        //      so it has already emitted the 'ready' event
         return redis
       })
   }
 
   return { createClient }
+}
+
+function waitForReadyPromise(redisClient) {
+  return new Promise((resolve, reject) => {
+    redisClient.on('ready', () => {
+      resolve(redisClient)
+    })
+    redisClient.on('error', (e) => {
+      reject(e)
+    })
+  })
 }
--- tests/spec.js
+++ tests/spec.js
@@ -4,6 +4,7 @@ const expect = chai.expect;
 const redisClientFactoryInit = require('../app')
 
 describe('redisClientFactoryInit tests', function() {
+  this.timeout('5s')
   it('connects to redis single instance mode', async () => {
     let redisClientFactory = redisClientFactoryInit({ host: 'localhost' })
     let redis = await redisClientFactory.createClient()
@@ -19,6 +20,31 @@ describe('redisClientFactoryInit tests', function() {
       port: 7000
     })
     let redis = await redisClientFactory.createClient()
+    await redis.set('took123', 1);
+    let took = await redis.get('took123')
+    expect(took).to.eql('1')
+    let nodes = redis.nodes()
+    await Promise.all(nodes.map((node) => {
+      node.disconnect()
+    }))
+    redis.disconnect()
+  })
+
+  it('connects to redis single instance mode with waitForReady flag', async () => {
+    let redisClientFactory = redisClientFactoryInit({ host: 'localhost' })
+    let redis = await redisClientFactory.createClient({ waitForReady: true })
+    await redis.set('took123', 1);
+    let took = await redis.get('took123')
+    expect(took).to.eql('1')
+    redis.quit()
+  })
+
+  it('connects to redis cluster mode with waitForReady flag', async () => {
+    let redisClientFactory = redisClientFactoryInit({
+      host: 'localhost',
+      port: 7000
+    })
+    let redis = await redisClientFactory.createClient({ waitForReady: true })
     await redis.set('took123', 1);
     let took = await redis.get('took123')
     expect(took).to.eql('1')
